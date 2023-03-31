d62c8861137e6acdd413406706b81691975fe780
NathanielBampo <mergontv@gmail.com>
2023-03-31 13:17:18 +0400

    fix Article's model and views

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,13 +1,23 @@
 class Article < ApplicationRecord
+  belongs_to :user, optional: true
+
+  before_save :link_author
+
   validates :title, presence: :true,
-                    length: { minimum: 1, maximum: 255 }
+                    length: { maximum: 255 }
 
   validates :body, presence: :true,
-                    length: { minimum: 1, maximum: 255 }
+                    length: { maximum: 255 }
   
   validates :author, presence: :true,
-                    length: { minimum: 1, maximum: 255 },
+                    length: { maximum: 255 },
                     format: { with: /\A[a-zA-Z0-9_]+\z/ }
   
   validates :datetime, presence: :true
+
+
+  def link_author
+    # Findes a user which haves nick == author
+    user = User.where(nick: self.author).first
+  end
 end
